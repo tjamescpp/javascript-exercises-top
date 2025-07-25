7238ae4fa61103f0780e1598fa3fdf118263f0cc
ttjamescpp <jamestommy.611@gmail.com>
2025-07-25 01:14:59 -0700

    completed object exercise 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,33 +1,43 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+    return arr.reduce((total, currentValue) => total + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+    return arr.reduce((total, currentValue) => total * currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+    return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+    let fact = a;
+
+    if (a === 0 || a === 1) {
+        return 1;
+    } else {
+        while (a > 1) {
+            fact *= a - 1;
+            a--;
+        }
+        return fact;
+    }
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
